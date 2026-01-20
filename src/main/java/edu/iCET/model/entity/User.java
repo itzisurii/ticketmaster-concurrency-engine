18ba2fad5a7afc33240feb3fe540517aa2ad379c
itzisurii <itzIsurika08@gmail.com>
2026-01-20 09:46:05 +0530

    create User entity with JPA annotations

---
+++ src/main/java/edu/iCET/model/entity/User.java
@@ -0,0 +1,24 @@
+package edu.iCET.model.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String tier;
+    private String email;
+
+}
