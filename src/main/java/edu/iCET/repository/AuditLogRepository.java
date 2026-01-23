0e380af37d9eadb98b89e834d1fce7d58bc56be9
itzisurii <itzIsurika08@gmail.com>
2026-01-23 11:38:47 +0530

    Add AuditLogRepository interface

---
+++ src/main/java/edu/iCET/repository/AuditLogRepository.java
@@ -0,0 +1,8 @@
+package edu.iCET.repository;
+
+import edu.iCET.model.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {}
