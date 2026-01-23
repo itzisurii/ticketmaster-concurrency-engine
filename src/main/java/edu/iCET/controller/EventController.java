2bb69ff7ff10e3edcb18eeb68d38c7034e602e2b
itzisurii <itzIsurika08@gmail.com>
2026-01-23 11:22:10 +0530

    Implement EventController with GET /event endpoint

---
+++ src/main/java/edu/iCET/controller/EventController.java
@@ -0,0 +1,24 @@
+package edu.iCET.controller;
+
+import edu.iCET.model.dto.EventDTO;
+import edu.iCET.model.entity.Event;
+import edu.iCET.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/event")
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping
+    public List<EventDTO> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+
+
+}
