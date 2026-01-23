92b8ef6fe822a2e9c623eb0be8dc0c65c6840529
itzisurii <itzIsurika08@gmail.com>
2026-01-23 11:52:38 +0530

    Add UserRepository interface

--- src/main/java/edu/iCET/repository/UserRepository.java
+++ src/main/java/edu/iCET/repository/UserRepository.java
@@ -2,6 +2,8 @@ package edu.iCET.repository;
 
 import edu.iCET.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
