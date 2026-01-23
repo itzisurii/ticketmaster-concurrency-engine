72d1c47564c704c6aed7592ec234486cad6eb434
itzisurii <itzIsurika08@gmail.com>
2026-01-23 11:49:45 +0530

    Add UserService with getAllUsers method

---
+++ src/main/java/edu/iCET/service/UserService.java
@@ -0,0 +1,35 @@
+package edu.iCET.service;
+
+import edu.iCET.model.dto.UserDTO;
+import edu.iCET.model.entity.User;
+import edu.iCET.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<UserDTO> getAllUsers() {
+        List<User> userList = userRepository.findAll();
+
+        List<UserDTO> userDTOS = new ArrayList<>();
+
+        for (User user : userList){
+            userDTOS.add(new UserDTO(
+                    user.getId(),
+                    user.getName(),
+                    user.getTier(),
+                    user.getEmail()
+            ));
+        }
+        return userDTOS;
+    }
+
+
+}
